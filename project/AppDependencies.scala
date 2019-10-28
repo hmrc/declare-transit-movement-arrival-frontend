d0341eeb83694774a3074f0e94a0f9904f6d2ebd
Nick Wilson <thenickwilson@gmail.com>
2019-10-28 17:10:11 +0000

    Bump version of govuk-frontend

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-whitelist-filter"          % "2.0.0",
     "uk.gov.hmrc"       %% "play-nunjucks-spike"            % "0.12.0-play-26",
     "uk.gov.hmrc"       %% "play-nunjucks-viewmodel-spike"  % "0.1.0-SNAPSHOT",
-    "org.webjars.npm"   %  "govuk-frontend"                 % "3.1.0"
+    "org.webjars.npm"   %  "govuk-frontend"                 % "3.3.0"
   )
 
   val test = Seq(
