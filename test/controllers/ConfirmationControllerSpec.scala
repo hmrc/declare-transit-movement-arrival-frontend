f1c8f65cd6b4bb89ce520948412d1198b89e2066
garethgooch <31101509+garethgooch@users.noreply.github.com>
2019-11-25 18:11:09 +0000

    further refactor and adding in test for removal of data

--- test/controllers/ConfirmationControllerSpec.scala
+++ test/controllers/ConfirmationControllerSpec.scala
@@ -26,6 +26,8 @@ import play.api.libs.json.{JsObject, Json}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.twirl.api.Html
+import repositories.SessionRepository
+import play.api.inject.bind
 
 import scala.concurrent.Future
 
@@ -33,12 +35,16 @@ class ConfirmationControllerSpec extends SpecBase with MockitoSugar with JsonMat
 
   "Confirmation Controller" - {
 
-    "return OK and the correct view for a GET" in {
+    "return OK and the correct view for a GET then remove data" in {
 
       when(mockRenderer.render(any(), any())(any()))
         .thenReturn(Future.successful(Html("")))
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val mockSessionRepository = mock[SessionRepository]
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[SessionRepository].toInstance(mockSessionRepository))
+        .build()
       val request = FakeRequest(GET, routes.ConfirmationController.onPageLoad(mrn).url)
       val templateCaptor = ArgumentCaptor.forClass(classOf[String])
       val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
@@ -48,6 +54,7 @@ class ConfirmationControllerSpec extends SpecBase with MockitoSugar with JsonMat
       status(result) mustEqual OK
 
       verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+      verify(mockSessionRepository,times(1)).remove(mrn.toString)
 
       val expectedJson = Json.obj("mrn" -> mrn)
 
