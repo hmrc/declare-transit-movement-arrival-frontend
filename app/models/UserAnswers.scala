d8d5c5dca038d208a4a137eea5b247ef4dfbadee
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2019-12-06 17:57:03 +0000

    Scaffold - Create mechanism to derive information based on UserAnswers.
    This introduces a way to get data from UserAnswers and also use a
    function to derive some meaningful data from the retreived answer
    using a function that processes the answer.

--- app/models/UserAnswers.scala
+++ app/models/UserAnswers.scala
@@ -18,6 +18,7 @@ package models
 
 import java.time.LocalDateTime
 
+import computable.Derivable
 import pages._
 import play.api.libs.json._
 import queries.Gettable
@@ -28,6 +29,9 @@ final case class UserAnswers(id: MovementReferenceNumber, data: JsObject = Json.
 
   def get[A](gettable: Gettable[A])(implicit rds: Reads[A]): Option[A] =
     Reads.optionNoError(Reads.at(gettable.path)).reads(data).getOrElse(None)
+
+  def get[A, B](derivable: Derivable[A, B])(implicit rds: Reads[A]): Option[B] =
+    get(derivable: Gettable[A]).map(derivable.derive)
 
   def set[A](page: QuestionPage[A], value: A)(implicit writes: Writes[A]): Try[UserAnswers] = {
 
