d8ee8ff87ed92bba4307bb0d73558646f83852a9
Gareth Gooch <31101509+garethgooch@users.noreply.github.com>
2019-12-23 16:00:28 +0000

    CTCTRADERS-143 Refactor ContainerNumberPage to use ContainerGettable path.
    Co-authored-by: Gareth Gooch <31101509+garethgooch@users.noreply.github.com>

--- app/pages/events/transhipments/ContainerNumberPage.scala
+++ app/pages/events/transhipments/ContainerNumberPage.scala
@@ -16,13 +16,14 @@
 
 package pages.events.transhipments
 
+import gettables.ContainerGettable
 import pages.QuestionPage
 import pages.events.SectionConstants
 import play.api.libs.json.JsPath
 
 final case class ContainerNumberPage(eventIndex: Int, containerIndex: Int) extends QuestionPage[String] {
 
-  override def path: JsPath = JsPath \ SectionConstants.events \ eventIndex \ SectionConstants.containers \ containerIndex \ toString
+  override def path: JsPath = ContainerGettable(eventIndex, containerIndex).path \ toString
 
   override def toString: String = "containerNumber"
 }
