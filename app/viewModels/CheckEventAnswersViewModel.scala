c9b02fa91699c91adf0a38c5aceca5fe8257d6dd
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2019-12-30 17:38:25 +0000

    CTCTRADERS-143 Move section generation logic from Check Event Answers controller to a view model.
    - Move logic out of controller to a view model
    - Add tests for view model
    - Expand on view model logic to display sections for transhipments

---
+++ app/viewModels/CheckEventAnswersViewModel.scala
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewModels
+
+import derivable.DeriveNumberOfContainers
+import models.TranshipmentType._
+import models.UserAnswers
+import pages.events.IsTranshipmentPage
+import pages.events.transhipments.TranshipmentTypePage
+import play.api.i18n.Messages
+import play.api.libs.json.{Json, OWrites}
+import uk.gov.hmrc.viewmodels.NunjucksSupport
+import uk.gov.hmrc.viewmodels.SummaryList.Row
+import utils.{AddContainerHelper, CheckYourAnswersHelper}
+
+case class CheckEventAnswersViewModel(eventInfo: Section, otherInfo: Seq[Section])
+
+object CheckEventAnswersViewModel extends NunjucksSupport {
+
+  def apply(userAnswers: UserAnswers, index: Int): CheckEventAnswersViewModel = {
+    val helper = new CheckYourAnswersHelper(userAnswers)
+
+    val eventInfoIsTranshipmentRow: Option[Row] = userAnswers.get(IsTranshipmentPage(index)) flatMap {
+      case false => helper.isTranshipment(index)
+      case true  => None
+    }
+
+    val eventInfo: Seq[Row] =
+      Seq(
+        helper.eventCountry(index),
+        helper.eventPlace(index),
+        helper.eventReported(index),
+        eventInfoIsTranshipmentRow,
+        helper.incidentInformation(index)
+      ).flatten
+
+    val otherInfoIsTranshipmentRow: Option[Row] = userAnswers.get(IsTranshipmentPage(index)) flatMap {
+      case false => None
+      case true  => helper.isTranshipment(index)
+    }
+
+    val differentVehicleSection: Section =
+      Section(
+        msg"checkEventAnswers.section.title.differentVehicle",
+        Seq(
+          otherInfoIsTranshipmentRow,
+          helper.transhipmentType(index),
+          helper.transportIdentity(index),
+          helper.transportNationality(index)
+        ).flatten
+      )
+
+    val addContainerHelper = AddContainerHelper(userAnswers)
+    val containers: Option[Section] = userAnswers
+      .get(DeriveNumberOfContainers(index))
+      .map(List.range(0, _))
+      .map(_.flatMap(addContainerHelper.containerRow(index, _)))
+      .map(Section.apply(msg"checkEventAnswers.section.title.containerNumbers", _))
+
+    val otherInfoSections: Seq[Section] = (userAnswers.get(IsTranshipmentPage(index)), userAnswers.get(TranshipmentTypePage(index))) match {
+      case (Some(true), Some(DifferentVehicle)) => Seq(differentVehicleSection)
+      case (Some(true), Some(DifferentContainer)) =>
+        Seq(
+          Some(
+            Section(
+              msg"checkEventAnswers.section.title.differentContainer",
+              Seq(
+                otherInfoIsTranshipmentRow,
+                helper.transhipmentType(index)
+              ).flatten
+            )
+          ),
+          containers
+        ).flatten
+      case (Some(true), Some(DifferentContainerAndVehicle)) =>
+        Seq(
+          Some(
+            Section(
+              msg"checkEventAnswers.section.title.differentContainerAndVehicle",
+              Seq(
+                otherInfoIsTranshipmentRow,
+                helper.transhipmentType(index)
+              ).flatten
+            )
+          ),
+          containers,
+          Some(
+            Section(
+              msg"checkEventAnswers.section.title.vehicleInformation",
+              Seq(
+                helper.transportIdentity(index),
+                helper.transportNationality(index)
+              ).flatten
+            )
+          )
+        ).flatten
+
+      case _ =>
+        Seq.empty
+    }
+
+    CheckEventAnswersViewModel(
+      Section(eventInfo),
+      otherInfoSections
+    )
+  }
+
+  implicit def writes(implicit messages: Messages): OWrites[CheckEventAnswersViewModel] = Json.writes[CheckEventAnswersViewModel]
+}
---
+++ test/viewModels/CheckEventAnswersViewModelSpec.scala
@@ -0,0 +1,230 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewModels
+
+import base.SpecBase
+import generators.DomainModelGenerators
+import models.TranshipmentType
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import pages.IncidentOnRoutePage
+import pages.events.transhipments.{ContainerNumberPage, TranshipmentTypePage, TransportIdentityPage, TransportNationalityPage}
+import pages.events._
+import play.api.libs.json.{JsObject, Json}
+import uk.gov.hmrc.viewmodels.SummaryList.Row
+
+class CheckEventAnswersViewModelSpec extends SpecBase with ScalaCheckPropertyChecks with DomainModelGenerators {
+  "must be able to deserialize to a JsObject" in {
+    val vm = CheckEventAnswersViewModel(Section(Seq.empty[Row]), Seq(Section(Seq.empty[Row])))
+
+    Json.toJsObject(vm) mustBe a[JsObject]
+  }
+
+  "when event is an incident" - {
+    "and hasn't been reported and did not move to different vehicle" in {
+      val ua = emptyUserAnswers
+        .set(IncidentOnRoutePage, true)
+        .success
+        .value
+        .set(EventCountryPage(eventIndex), "value")
+        .success
+        .value
+        .set(EventPlacePage(eventIndex), "value")
+        .success
+        .value
+        .set(EventReportedPage(eventIndex), false)
+        .success
+        .value
+        .set(IsTranshipmentPage(eventIndex), false)
+        .success
+        .value
+        .set(IncidentInformationPage(eventIndex), "value")
+        .success
+        .value
+
+      val vm = CheckEventAnswersViewModel(ua, eventIndex)
+
+      vm.eventInfo.sectionTitle must not be defined
+      vm.eventInfo.rows.length mustEqual 5
+      vm.otherInfo must be(empty)
+    }
+
+    "and has been reported and did not move to different vehicle" in {
+      val ua = emptyUserAnswers
+        .set(IncidentOnRoutePage, true)
+        .success
+        .value
+        .set(EventCountryPage(eventIndex), "value")
+        .success
+        .value
+        .set(EventPlacePage(eventIndex), "value")
+        .success
+        .value
+        .set(EventReportedPage(eventIndex), true)
+        .success
+        .value
+        .set(IsTranshipmentPage(eventIndex), false)
+        .success
+        .value
+
+      val vm = CheckEventAnswersViewModel(ua, eventIndex)
+
+      vm.eventInfo.sectionTitle must not be defined
+      vm.eventInfo.rows.length mustEqual 4
+      vm.otherInfo must be(empty)
+    }
+  }
+
+  "when event is a transhipment" - {
+    "and the goods have moved to different vehicle" in {
+      val ua = emptyUserAnswers
+        .set(IncidentOnRoutePage, true)
+        .success
+        .value
+        .set(EventCountryPage(eventIndex), "value")
+        .success
+        .value
+        .set(EventPlacePage(eventIndex), "value")
+        .success
+        .value
+        .set(EventReportedPage(eventIndex), false)
+        .success
+        .value
+        .set(IsTranshipmentPage(eventIndex), true)
+        .success
+        .value
+        .set(TranshipmentTypePage(eventIndex), TranshipmentType.DifferentVehicle)
+        .success
+        .value
+        .set(TransportIdentityPage(eventIndex), "value")
+        .success
+        .value
+        .set(TransportNationalityPage(eventIndex), "TT")
+        .success
+        .value
+
+      val vm = CheckEventAnswersViewModel(ua, eventIndex)
+
+      vm.eventInfo.sectionTitle must not be defined
+      vm.eventInfo.rows.length mustEqual 3
+
+      vm.otherInfo.length mustEqual 1
+      vm.otherInfo.head.sectionTitle must be(defined)
+
+      vm.otherInfo.head.rows.length mustEqual 4
+    }
+
+    "and the goods have moved to different container" in {
+      val ua = emptyUserAnswers
+        .set(IncidentOnRoutePage, true)
+        .success
+        .value
+        .set(EventCountryPage(eventIndex), "value")
+        .success
+        .value
+        .set(EventPlacePage(eventIndex), "value")
+        .success
+        .value
+        .set(EventReportedPage(eventIndex), false)
+        .success
+        .value
+        .set(IsTranshipmentPage(eventIndex), true)
+        .success
+        .value
+        .set(TranshipmentTypePage(eventIndex), TranshipmentType.DifferentContainer)
+        .success
+        .value
+        .set(ContainerNumberPage(eventIndex, 0), "value")
+        .success
+        .value
+        .set(ContainerNumberPage(eventIndex, 1), "value")
+        .success
+        .value
+        .set(ContainerNumberPage(eventIndex, 2), "value")
+        .success
+        .value
+
+      val vm = CheckEventAnswersViewModel(ua, eventIndex)
+
+      vm.eventInfo.sectionTitle must not be defined
+      vm.eventInfo.rows.length mustEqual 3
+
+      vm.otherInfo.length mustEqual 2
+
+      vm.otherInfo.head.sectionTitle must be(defined)
+      vm.otherInfo.head.rows.length mustEqual 2
+
+      vm.otherInfo(1).sectionTitle must be(defined)
+      vm.otherInfo(1).rows.length mustEqual 3
+
+    }
+
+    "and the goods have moved to both different containers and vehicles" in {
+      val ua = emptyUserAnswers
+        .set(IncidentOnRoutePage, true)
+        .success
+        .value
+        .set(EventCountryPage(eventIndex), "value")
+        .success
+        .value
+        .set(EventPlacePage(eventIndex), "value")
+        .success
+        .value
+        .set(EventReportedPage(eventIndex), false)
+        .success
+        .value
+        .set(IsTranshipmentPage(eventIndex), true)
+        .success
+        .value
+        .set(TranshipmentTypePage(eventIndex), TranshipmentType.DifferentContainerAndVehicle)
+        .success
+        .value
+        .set(ContainerNumberPage(eventIndex, 0), "value")
+        .success
+        .value
+        .set(ContainerNumberPage(eventIndex, 1), "value")
+        .success
+        .value
+        .set(ContainerNumberPage(eventIndex, 2), "value")
+        .success
+        .value
+        .set(TransportIdentityPage(eventIndex), "value")
+        .success
+        .value
+        .set(TransportNationalityPage(eventIndex), "TT")
+        .success
+        .value
+
+      val vm = CheckEventAnswersViewModel(ua, eventIndex)
+
+      vm.eventInfo.sectionTitle must not be defined
+      vm.eventInfo.rows.length mustEqual 3
+
+      vm.otherInfo.length mustEqual 3
+
+      vm.otherInfo.head.sectionTitle must be(defined)
+      vm.otherInfo.head.rows.length mustEqual 2
+
+      vm.otherInfo(1).sectionTitle must be(defined)
+      vm.otherInfo(1).rows.length mustEqual 3
+
+      vm.otherInfo(2).sectionTitle must be(defined)
+      vm.otherInfo(2).rows.length mustEqual 2
+
+    }
+
+  }
+}
