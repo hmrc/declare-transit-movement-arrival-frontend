6d0e5c3f0731e77acbc550931bf9e1268447b22e
snehaprathipat1 <nihaq1@gmail.com>
2020-06-05 00:12:12 +0100

    Add xml reads and writes test for TraderDestination model

--- test/models/messages/TraderDestinationSpec.scala
+++ test/models/messages/TraderDestinationSpec.scala
@@ -16,11 +16,13 @@
 
 package models.messages
 
+import com.lucidchart.open.xtract.XmlReader
 import generators.MessagesModelGenerators
 import models.LanguageCodeEnglish
 import models.XMLWrites._
 import org.scalacheck.Arbitrary.arbitrary
 import org.scalatest.{FreeSpec, MustMatchers, StreamlinedXmlEquality}
+import org.scalatest.OptionValues._
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 import scala.xml.NodeSeq
@@ -78,6 +80,50 @@ class TraderDestinationSpec extends FreeSpec with MustMatchers with ScalaCheckPr
 
           traderDestination.toXml mustEqual expectedResult
       }
+    }
+
+    "must read xml into valid model" in {
+
+      forAll(arbitrary[TraderDestination]) {
+        traderDestination =>
+          val nameNode = traderDestination.name.map(
+            name => <NamTRD7>{name}</NamTRD7>
+          )
+          val streetNameNode = traderDestination.streetAndNumber.map(
+            streetName => <StrAndNumTRD22>{streetName}</StrAndNumTRD22>
+          )
+          val postCodeNode = traderDestination.postCode.map(
+            postcode => <PosCodTRD23>{postcode}</PosCodTRD23>
+          )
+          val cityNode = traderDestination.city.map(
+            city => <CitTRD24>{city}</CitTRD24>
+          )
+          val countryCodeNode = traderDestination.countryCode.map(
+            countryCode => <CouTRD25>{countryCode}</CouTRD25>
+          )
+          val eoriNode = traderDestination.eori.map(
+            eori => <TINTRD59>{eori}</TINTRD59>
+          )
+
+          val inputXml =
+            <TRADESTRD>
+            {
+            nameNode.getOrElse(NodeSeq.Empty) ++
+              streetNameNode.getOrElse(NodeSeq.Empty) ++
+              postCodeNode.getOrElse(NodeSeq.Empty) ++
+              cityNode.getOrElse(NodeSeq.Empty) ++
+              countryCodeNode.getOrElse(NodeSeq.Empty) ++
+              <NADLNGRD>{LanguageCodeEnglish.code}</NADLNGRD> ++
+              eoriNode.getOrElse(NodeSeq.Empty)
+            }
+          </TRADESTRD>
+
+          val result = XmlReader.of[TraderDestination].read(inputXml).toOption.value
+
+          result mustEqual TraderDestination
+
+      }
+
     }
 
   }
