d602bb4e41f697c19a03f56ff157581981d2c959
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2020-06-10 08:31:16 +0100

    CTCTRADERS-505: Add ArrivalMovementRequestConversionService

---
+++ test/services/conversion/ArrivalMovementRequestConversionServiceSpec.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services.conversion
+
+import base.SpecBase
+import generators.MessagesModelGenerators
+import models.MovementReferenceNumber
+import models.messages.{ArrivalMovementRequest, NormalNotification, TraderWithEori}
+import org.scalacheck.Gen
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class ArrivalMovementRequestConversionServiceSpec extends SpecBase with MessagesModelGenerators with ScalaCheckPropertyChecks {
+
+  val arrivalMovementRequestConversionService: ArrivalMovementRequestConversionService = app.injector.instanceOf[ArrivalMovementRequestConversionService]
+
+  "ArrivalMovementRequest" - {
+
+    "must convert ArrivalMovementRequest to NormalNotification for traders with Eori" in {
+      val notifications: Gen[(ArrivalMovementRequest, NormalNotification)] = {
+        for {
+          arrivalNotificationRequest <- arbitraryArrivalMovementRequestWithEori
+        } yield {
+
+          val normalNotification: NormalNotification = {
+            NormalNotification(
+              movementReferenceNumber = MovementReferenceNumber(arrivalNotificationRequest.header.movementReferenceNumber).get,
+              notificationPlace       = arrivalNotificationRequest.header.arrivalNotificationPlace,
+              notificationDate        = arrivalNotificationRequest.header.notificationDate,
+              customsSubPlace         = arrivalNotificationRequest.header.customsSubPlace,
+              trader = TraderWithEori(
+                name            = arrivalNotificationRequest.traderDestination.name,
+                streetAndNumber = arrivalNotificationRequest.traderDestination.streetAndNumber,
+                postCode        = arrivalNotificationRequest.traderDestination.postCode,
+                city            = arrivalNotificationRequest.traderDestination.city,
+                countryCode     = arrivalNotificationRequest.traderDestination.countryCode,
+                eori            = arrivalNotificationRequest.traderDestination.eori.value
+              ),
+              presentationOfficeId   = arrivalNotificationRequest.header.presentationOfficeId,
+              presentationOfficeName = arrivalNotificationRequest.header.presentationOfficeName,
+              enRouteEvents          = arrivalNotificationRequest.enRouteEvents
+            )
+          }
+
+          (arrivalNotificationRequest, normalNotification)
+        }
+      }
+
+      forAll(notifications) {
+
+        case (arrivalNotificationRequest, normalNotification) =>
+          arrivalMovementRequestConversionService.convertToArrivalNotification(arrivalNotificationRequest) mustBe normalNotification
+      }
+    }
+  }
+
+}
