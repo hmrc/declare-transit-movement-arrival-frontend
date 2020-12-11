bdaf0e53994b04fef425b2310a8680c13d7c5d7a
sawan07 <sawanbearn08@gmail.com>
2020-12-11 12:07:35 +0000

    debugging small issue

--- app/renderer/Renderer.scala
+++ app/renderer/Renderer.scala
@@ -44,7 +44,7 @@ class Renderer @Inject()(appConfig: FrontendAppConfig, renderer: NunjucksRendere
     "reportAProblemPartialUrl"       -> appConfig.reportAProblemPartialUrl,
     "reportAProblemNonJSUrl"         -> appConfig.reportAProblemNonJSUrl,
     "signOutUrl"                     -> appConfig.signOutUrl,
-    "manageTransitMovementsUrl"      -> appConfig.manageTransitMovementsUrl
+    "manageTransitMovementsUrl"      -> appConfig.manageTransitMovementsUrl,
     "timeout"                        -> appConfig.timeoutSeconds,
     "countdown"                      -> appConfig.countdownSeconds
   )
