42ab0d31f82b953588f43276cb528d8dce2a3455
Jay Nabonne <jaynabonne@gmail.com>
2021-09-30 16:02:48 +0100

    FIx warnings. Update some dependency versions.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo" %% "play2-reactivemongo"              % "0.20.11-play28",
-    "org.reactivemongo" %% "reactivemongo-play-json-compat"   % "0.20.11-play28",
+    "org.reactivemongo" %% "play2-reactivemongo"              % "0.20.13-play28",
+    "org.reactivemongo" %% "reactivemongo-play-json-compat"   % "0.20.13-play28",
     "com.typesafe.play" %% "play-iteratees"                   % "2.6.1",
     "com.typesafe.play" %% "play-iteratees-reactive-streams"  % "2.6.1",
     "uk.gov.hmrc"       %% "logback-json-logger"              % "5.1.0",
@@ -16,7 +16,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-allowlist-filter"            % "1.0.0-play-28",
     "uk.gov.hmrc"       %% "play-nunjucks"                    % "0.32.0-play-28",
     "uk.gov.hmrc"       %% "play-nunjucks-viewmodel"          % "0.15.0-play-28",
-    "org.webjars.npm"   % "govuk-frontend"                    % "3.10.1",
+    "org.webjars.npm"   % "govuk-frontend"                    % "3.13.0",
     "org.webjars.npm"   % "hmrc-frontend"                     % "1.22.0",
     "com.lucidchart"    %% "xtract"                           % "2.2.1"
   )
@@ -31,7 +31,7 @@ object AppDependencies {
     "org.jsoup"              % "jsoup"                  % "1.14.2",
     "com.typesafe.play"      %% "play-test"             % PlayVersion.current,
     "org.mockito"            % "mockito-all"            % "1.10.19",
-    "org.scalacheck"         %% "scalacheck"            % "1.15.0",
+    "org.scalacheck"         %% "scalacheck"            % "1.15.4",
     "com.github.tomakehurst" % "wiremock-standalone"    % "2.27.2",
     "wolfendale"             %% "scalacheck-gen-regexp" % "0.1.2"
   ).map(_ % "test, it")
