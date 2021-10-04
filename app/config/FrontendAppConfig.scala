3517a68da151fd9dfc124516b1aa72db6fa88c97
carrielies <carrielies@users.noreply.github.com>
2021-10-04 09:30:32 +0100

    CTCTRADERS-2607 TidyUp

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -44,7 +44,6 @@ class FrontendAppConfig @Inject()(configuration: Configuration) {
   lazy val authUrl: String            = configuration.get[Service]("auth").fullServiceUrl
   lazy val loginUrl: String           = configuration.get[String]("urls.login")
   lazy val loginContinueUrl: String   = configuration.get[String]("urls.loginContinue")
-  lazy val enrolmentKey: String       = configuration.get[String]("urls.enrolmentKey")
   lazy val destinationUrl: String     = configuration.get[Service]("microservice.services.destination").fullServiceUrl
   lazy val baseDestinationUrl: String = configuration.get[Service]("microservice.services.destination").baseUrl
   lazy val referenceDataUrl: String   = configuration.get[Service]("microservice.services.referenceData").fullServiceUrl
