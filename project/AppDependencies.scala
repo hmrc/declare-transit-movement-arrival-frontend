96f44a8f06d9d6c2dc6bb187649eec044bb9f4c1
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2023-05-15 09:35:05 +0100

    Updating hmrc-frontend and govuk-frontend dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"       % bootstrapVersion,
     "uk.gov.hmrc"          %% "play-allowlist-filter"            % "1.1.0",
     "uk.gov.hmrc"          %% "play-nunjucks"                    % "0.41.0-play-28",
-    "uk.gov.hmrc"          %% "play-nunjucks-viewmodel"          % "0.17.0-play-28",
+    "uk.gov.hmrc"          %% "play-nunjucks-viewmodel"          % "0.18.0-play-28",
     "org.webjars.npm"      %  "govuk-frontend"                   % "4.6.0",
     "uk.gov.hmrc.webjars"  %  "hmrc-frontend"                    % "5.31.0",
     "com.lucidchart"       %% "xtract"                           % "2.2.1"
